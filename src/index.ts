7c949c327f10a011dffa56aff27a7bddcc6e05c9
Vinayak-fr <Vinayak.patil@acelucid.com>
2025-12-25 19:43:37 +0530

    setup + packages installation

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import jwt from "jsonwebtoken"
+
+const app = express()
+
+app.post("/api/v1/signup",(req, res) =>{
+    
+})
+
+app.post("/api/v1/signin",(req, res) =>{
+
+})
+
+app.get("/api/v1/content",(req, res) =>{
+
+})
+
+app.post("/api/v1/space/share", (req,res) => {
+
+})
+
+app.get("/api/v1/brain/:sharelink", (req, res) => {
+    
+})
+
+console.log("Server is running")
